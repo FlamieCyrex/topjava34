947fd8c2bb922565081187fb8ffaf4e262a9cfe7
FlamieCyrex <wave-5@yandex.ru>
2025-03-16 17:43:01 +0300

    Add profiles for choose repositoryType and create Test for all of them

---
+++ src/test/java/ru/javawebinar/topjava/service/DataJpaMealServiceTest.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.topjava.service;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.ActiveDbProfileResolver;
+
+@ActiveProfiles(resolver = ActiveDbProfileResolver.class, profiles = "datajpa")
+public class DataJpaMealServiceTest extends MealServiceTest {
+
+}
